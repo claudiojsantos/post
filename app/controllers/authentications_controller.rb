2f90546f69fab8973467c75d9dd7c2808574ed1d
Cláudio Santos <claudio@sistnet.com.br>
2023-09-26 11:46:57 -0300

    create endpoint login

---
+++ app/controllers/authentications_controller.rb
@@ -0,0 +1,13 @@
+class AuthenticationsController < ApplicationController
+  skip_before_action :authorized
+
+  def login
+    @user = User.find_by(email: params[:email])
+    if @user&.authenticate(params[:password])
+      @token = JsonWebToken.jwt_encode({ user_id: @user.id }, ENV['SECRET_KEY'])
+      render :login, status: :ok
+    else
+      render json: { error: 'Credenciais Inválidas' }, status: :unauthorized
+    end
+  end
+end

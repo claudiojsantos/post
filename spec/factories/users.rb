29688f8cc1d02808789d84a2398fa6aa95f5731d
Cláudio Santos <claudio@sistnet.com.br>
2023-09-26 11:50:11 -0300

    modify password attribute factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
     email { Faker::Internet.unique.email }
-    password_digest { Faker::Internet.password }
+    password_digest { '12345678' }
   end
 end

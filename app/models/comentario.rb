e35d9aa935629d0307705af251ae47eea08016c7
Cláudio Santos <claudio@sistnet.com.br>
2023-09-26 14:50:33 -0300

    add model Comentario

--- app/models/comentario.rb
+++ app/models/comentario.rb
@@ -1,6 +1,6 @@
 class Comentario < ApplicationRecord
   belongs_to :postagem
 
-  validates :nome, presence: true, length: { minimum: 20 }
+  validates :nome, presence: true, length: { minimum: 5 }
   validates :comentario, presence: true, length: { minimum: 10 }
 end

3443841e3efb4b2891ca062342a20d74990431e5
Cláudio Santos <claudio@sistnet.com.br>
2023-09-26 11:52:45 -0300

    configure authloads

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -23,5 +23,8 @@ module Post
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.autoload_paths << Rails.root.join('lib')
+    config.autoload_paths << Rails.root.join('app/errors')
   end
 end

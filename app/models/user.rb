e22625eaffc09790c15794ba9a1389a1bb0c18c0
Cláudio Santos <claudio@sistnet.com.br>
2023-09-26 13:44:48 -0300

    add has_many postagens association test to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :postagens, dependent: :destroy
+
   validates :name, presence: true
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, on: :create }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, on: :create },
+                    uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 8, if: -> { password.present? } }
 end

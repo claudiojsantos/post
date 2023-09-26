a837d583133abb08e1c5e41c57ceb9d12963e422
Cláudio Santos <claudio@sistnet.com.br>
2023-09-26 18:02:14 -0300

    fix error quantity arguments

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -4,7 +4,7 @@ class AuthenticationsController < ApplicationController
   def login
     @user = User.find_by(email: params[:email])
     if @user&.authenticate(params[:password])
-      @token = JsonWebToken.jwt_encode({ user_id: @user.id }, ENV['SECRET_KEY'])
+      @token = JsonWebToken.jwt_encode({ user_id: @user.id })
       render :login, status: :ok
     else
       render json: { error: 'Credenciais Inválidas' }, status: :unauthorized

c6a0165e1806275a407ef4c25a24e5e628554947
Cláudio Santos <claudio@sistnet.com.br>
2023-09-26 17:57:30 -0300

    create a concern to handle the viewing control of request responses

---
+++ app/controllers/concerns/paginatable.rb
@@ -0,0 +1,25 @@
+module Paginatable
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :check_page_range, if: :paginatable_model
+  end
+
+  protected
+
+  def check_page_range
+    total_pages = total_pages_for(paginatable_model)
+    return unless params[:page].to_i > total_pages
+
+    params[:page] = total_pages
+  end
+
+  def total_pages_for(model, per_page = 5)
+    total_count = model.count
+    (total_count.to_f / per_page).ceil
+  end
+
+  def paginatable_model
+    nil
+  end
+end

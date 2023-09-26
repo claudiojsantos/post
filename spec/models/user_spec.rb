524b7e31a4ab28fa3ee2ef7732efcddec245a5fe
Cláudio Santos <claudio@sistnet.com.br>
2023-09-26 14:51:18 -0300

    add test of association

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
     it { should validate_presence_of(:password) }
     it { should validate_length_of(:password).is_at_least(8) }
+
+    it { should have_many(:postagens).dependent(:destroy) }
   end
 
   describe 'other validations' do

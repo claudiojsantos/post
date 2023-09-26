fdfafa61a2972304aadab18424837a2d087c199d
Cláudio Santos <claudio@sistnet.com.br>
2023-09-26 18:35:56 -0300

    added condition for short text titles

--- spec/factories/postagens.rb
+++ spec/factories/postagens.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :postagem do
-    titulo { Faker::Book.title }
+    titulo { Faker::Book.title.length < 10 ? Faker::Book.title + Faker::Book.title : Faker::Book.title }
     texto { Faker::Lorem.paragraphs(number: 3).join("\n\n") }
     user
   end

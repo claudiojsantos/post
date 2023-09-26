f69f1c270ef09b3a5b38aa848f9edacd0f890f2c
Cláudio Santos <claudio@sistnet.com.br>
2023-09-26 14:53:28 -0300

    added seed data for postagens and comentarios

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
+include FactoryBot::Syntax::Methods
+
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 puts 'Seeding database...'
-User.find_or_create_by!(name: 'admin', email: 'admin@test.com') do |user|
+
+author = User.find_or_create_by!(name: 'author', email: 'author@test.com') do |user|
   user.password = '12345678'
 end
+
+10.times do
+  postagem = create(:postagem, user: author)
+
+  5.times do
+    create(:comentario, postagem:)
+  end
+end
+
 puts 'Seeding complete!'

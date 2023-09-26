9d0ce4ce43cec93a9fed00faf4152c3f4aee2b7f
Cláudio Santos <claudio@sistnet.com.br>
2023-09-26 18:00:01 -0300

    add validator of exp variable

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -3,6 +3,8 @@ class JsonWebToken
 
   class << self
     def jwt_encode(payload, exp = 1.day.from_now)
+      raise ArgumentError, 'exp should be a Time or DateTime object' unless exp.is_a?(Time) || exp.is_a?(DateTime)
+
       payload[:exp] = exp.to_i
       JWT.encode(payload, SECRET_KEY, 'HS256')
     end

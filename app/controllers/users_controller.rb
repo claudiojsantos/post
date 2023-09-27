f5052142a0949915f24857112eb5e7586ef8b5e4
Cláudio Santos <claudio@sistnet.com.br>
2023-09-27 03:12:52 -0300

    create put for user endpoint for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,30 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find_by(id: params[:id])
+
+    return render json: { errors: 'Usuário inexistente' }, status: :not_found if @user.nil?
+
+    @service = User::UpdateService.new(@user, user_params)
+
+    if @service.call
+      render json: { message: 'Usuário Atualizado' }, status: :ok
+    else
+      render json: { errors: @service.errors }, status: :unprocessable_entity
+    end
   end
 
-  def create
+  def create; end
+
+  def delete; end
+
+  private
+
+  def paginatable_model
+    User
   end
 
-  def delete
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end

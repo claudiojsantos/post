3b6de8ee6c8d2a16a9631c6b595539ab6a94564e
Cláudio Santos <claudio@sistnet.com.br>
2023-09-26 23:57:34 -0300

    modify lenght text titulo

--- app/models/postagem.rb
+++ app/models/postagem.rb
@@ -2,6 +2,6 @@ class Postagem < ApplicationRecord
   belongs_to :user
   has_many :comentarios, dependent: :destroy
 
-  validates :titulo, presence: true, length: { minimum: 10 }
+  validates :titulo, presence: true, length: { minimum: 5 }
   validates :texto, presence: true, length: { minimum: 10 }
 end
--- spec/models/postagem_spec.rb
+++ spec/models/postagem_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Postagem, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:titulo) }
-    it { should validate_length_of(:titulo).is_at_least(10) }
+    it { should validate_length_of(:titulo).is_at_least(5) }
     it { should validate_presence_of(:texto) }
     it { should validate_length_of(:texto).is_at_least(10) }
   end

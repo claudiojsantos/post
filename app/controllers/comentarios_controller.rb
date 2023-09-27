e89ca14c64c9d55ff3e8393582f38e2b1fba4516
Cláudio Santos <claudio@sistnet.com.br>
2023-09-27 11:39:46 -0300

    create post endpoint for comentarios

--- app/controllers/comentarios_controller.rb
+++ app/controllers/comentarios_controller.rb
@@ -1,4 +1,6 @@
 class ComentariosController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
   def index
     @comentarios = current_user.comentarios.page(params[:page]).per(5)
   end
@@ -7,5 +9,23 @@ class ComentariosController < ApplicationController
     @comentario = Comentario.where(id: params[:id]).page(params[:page]).per(5)
   end
 
-  def create; end
+  def create
+    @comentario = Comentario.new(comentario_params)
+
+    if Comentario::CreateService.new(@comentario).call
+      render json: { message: 'Comentário Criada com Sucesso' }, status: :created
+    else
+      render json: { errors: @comentario.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def paginatable_model
+    Comentario
+  end
+
+  def comentario_params
+    params.require(:comentario).permit(:nome, :comentario, :postagem_id)
+  end
 end

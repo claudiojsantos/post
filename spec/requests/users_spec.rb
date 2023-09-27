88e2cab34e4cb038572c8220d6a78e40d746636f
Cláudio Santos <claudio@sistnet.com.br>
2023-09-27 02:44:12 -0300

    tests to endpoint get show user

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -34,12 +34,26 @@ RSpec.describe 'Users', type: :request do
     end
   end
 
-  # describe 'GET /show' do
-  #   it 'returns http success' do
-  #     get '/users/show'
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe 'GET /users/:id' do
+    let(:current_user) { create(:user) }
+    let(:other_user) { create(:user) }
+    let!(:user) { create(:user) }
+    let(:url) { "#{base_url}/users/#{user.id}" }
+    let(:other_user) { create(:user) }
+    let(:other_url) { "#{base_url}/users/#{user.id}" }
+
+    context 'verify authorization' do
+      it 'return 401 status if user is not logged in' do
+        get url
+        expect(response).to custom_have_http_status(401)
+      end
+    end
+
+    it 'assigns the requested user to @user' do
+      get url, headers: login_as(current_user)
+      expect(assigns(:user)).to match_array([user])
+    end
+  end
 
   # describe 'GET /update' do
   #   it 'returns http success' do
